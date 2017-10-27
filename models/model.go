b643642fe3fb1334797504d22406e35acb5b0ff4
崔士川 <cuisc@bkread.cn>
2017-10-28 06:31:11 +0800

    db

--- models/model.go
+++ models/model.go
@@ -17,13 +17,13 @@ func init(){
 	var err error
 	_Client, err = ssdb.Connect(conf.DBCONFIG.Host, conf.DBCONFIG.Port)
 	if err != nil {
-		panic(err)
+		//panic(err)
 	}
 	log.Println("Connetct to ssdb successed!")
 
 	_MgoSession, err = mgo.Dial(fmt.Sprintf("%s:%d", conf.MGOCONF.Host, conf.MGOCONF.Port))
 	if err != nil {
-		panic(err)
+		//panic(err)
 	}
 	log.Println("Connect to mongo successed!")
 
